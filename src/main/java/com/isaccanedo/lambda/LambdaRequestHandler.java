b4d6e7eb0d5d7434694886ec70be25457a021ad8
isaccanedo <isaccanedo@gmail.com>
2021-11-06 17:21:37 -0300

    Initial commit

---
+++ src/main/java/com/isaccanedo/lambda/LambdaRequestHandler.java
@@ -0,0 +1,11 @@
+package com.isaccanedo.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaRequestHandler implements RequestHandler<String, String> {
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
+}
